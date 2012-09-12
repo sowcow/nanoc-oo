477c5349ff660745228f3fc2b09503a2fef0ce4c
Alexander K <xpyro@ya.ru>
2012-09-12 14:40:16 +0400

    rspec focus

--- spec/support/1_config.rb
+++ spec/support/1_config.rb
@@ -4,3 +4,13 @@ WRAP = 'wrapper'
 def require_lib
   require File.expand_path "#{WRAP}/lib/all.rb"
 end
+
+RSpec.configure do |config|
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+end
+
+def focus
+  :focus
+end
