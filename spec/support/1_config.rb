448e352f8713724e7596ae0c807a98952b681975
Alexander K <xpyro@ya.ru>
2012-09-21 13:28:49 +0400

    require classes helper

--- spec/support/1_config.rb
+++ spec/support/1_config.rb
@@ -5,6 +5,11 @@ def require_lib
   require File.expand_path "#{WRAP}/lib/all.rb"
 end
 
+def require_classes
+  Dir["#{WRAP}/lib/classes/*.rb"].each {|file| require File.expand_path file }
+end
+
+
 RSpec.configure do |config|
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
