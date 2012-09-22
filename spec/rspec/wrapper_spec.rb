ddb213894f7d677439b5150d8fdd122ba34fec14
Alexander K <xpyro@ya.ru>
2012-09-22 17:01:21 +0400

    should add extensions to config.yaml

--- spec/rspec/wrapper_spec.rb
+++ spec/rspec/wrapper_spec.rb
@@ -8,4 +8,12 @@ describe 'wrapper' do
   it 'should delete lib/all file' do
     File.should_not exist "#{SITE}/lib/all.rb"
   end  
+  
+  it 'should add textual file extensions (.slim) to config.yaml' do
+    require 'yaml'
+    filters = YAML.load(File.read("#{SITE}/config.yaml"))['text_extensions']
+    filters.should be_a Array
+    filters.should include 'erb'
+    filters.should include 'slim'
+  end    
 end
--- spec/support/3_wrapper.rb
+++ spec/support/3_wrapper.rb
@@ -4,8 +4,15 @@ def wrap
   File.delete "#{SITE}/lib/all.rb"
 end
 
+def add_filters filters, config = "#{SITE}/config.yaml"
+  text = File.read(config)
+  text[/text_extensions: ?\[()/, 1] = filters.map{|x| " '#{x}'," }.join
+  File.write config, text
+end
+
 RSpec.configure do |config|
   config.before(:all) do
     wrap
+    add_filters ['slim']
   end
 end
