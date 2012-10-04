2ed2dc90714b15d6c82fc99f5b9f1a8ded22e1ec
Alexander K <xpyro@ya.ru>
2012-10-04 12:21:37 +0400

    small tree compiled guided by #children

--- spec/item_children_and_comiling_spec.rb
+++ spec/item_children_and_comiling_spec.rb
@@ -1,6 +1,18 @@
 require 'support/all'
 require_lib
 
+def chdir name
+  Dir.mkdir name
+  Dir.chdir name do
+    yield
+  end
+end
+
+def write file, text=lorem
+  File.write file, text
+end
+
+
 
 describe 'items compiling guided by children' do
   before do
@@ -48,4 +60,38 @@ describe 'items compiling guided by children' do
       File.should exist output name
     end
   end
+
+  context 'small tree of items with index.html files' do
+
+    let(:name){'some-temp-test-dir'}
+    let(:dir){"#{SITE}/content/#{name}"}
+    let(:text){ lorem }
+    after :all do
+      rm_rf dir
+    end
+    before :all do
+      chdir dir do
+        write 'index.html'
+        write 'file1.html'
+        chdir 'dir' do
+          write 'index.html'
+          write 'file2.html'
+          write 'file3.html'
+        end
+      end
+    end
+
+    it 'compiles nested files' do
+      compile!
+      File.should exist output "#{name}/file1"
+      File.should exist output "#{name}/dir/file2"
+      File.should exist output "#{name}/dir/file3"
+    end
+
+    it 'compiles index items' do
+      compile!
+      File.should exist output name
+      File.should exist output "#{name}/dir"
+    end
+  end  
 end
