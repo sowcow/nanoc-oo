ed5b9f9dc2bcc94161ad3c31c2cc5677bd1a6286
Alexander K <xpyro@ya.ru>
2012-10-05 12:32:26 +0400

    splitted steps

---
+++ features/step_definitions/nanoc_steps.rb
@@ -0,0 +1,18 @@
+When /^~nanoc (.*)$/ do |command|
+  step "I run `ruby -S nanoc #{command}`"
+end
+
+Given 'I have a default nanoc site' do
+  step '~nanoc create-site site'
+  step 'I cd to "site"'
+end
+
+When 'I successfully compile it' do
+  step '~nanoc compile'
+  step 'it is successfully compiled'
+end
+
+Then 'it is successfully compiled' do
+  step 'a directory named "output" should exist'
+  step 'the file "crash.log" should not exist'
+end
