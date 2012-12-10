8056a3f39ef61855b3c0170f4a22c9171f82f467
Alexander K <xpyro@ya.ru>
2012-12-10 20:39:19 +0600

    now copy file routines using Thor...

--- features/cli.feature
+++ features/cli.feature
@@ -1,6 +1,33 @@
 Feature: command line interface
+
   Scenario: nanoc output
-    Given $ nanoc-oo mysite
+    When $ nanoc-oo mysite
     Then the output should contain "Enjoy!"
     When $ nanoc-oo mysite
     Then the output should contain "already exists"
+
+  Scenario: (no side effect between features I)
+    When $ nanoc-oo thesite
+    Then the output should contain "Enjoy!"
+  Scenario: (no side effect between features II)
+    When $ nanoc-oo thesite
+    Then the output should contain "Enjoy!"    
+
+  Scenario: create site
+    When $ nanoc-oo site
+    Then a file named "site/Rules" should exist
+    And a file named "site/content/index.html" should exist
+
+  Scenario: create blank site
+    When $ nanoc-oo site --blank
+    Then a file named "site/Rules" should exist
+    And a file named "site/content/index.html" should not exist  
+
+  Scenario: Rules file is replaced
+    When $ nanoc-oo site
+    Then the file "site/Rules" should contain "FakeItem"
+
+  Scenario: it describes actions
+    When $ nanoc-oo thesite
+    Then the output should contain "force"
+    Then the output should contain "classes"
--- lib/nanoc-oo.rb
+++ lib/nanoc-oo.rb
@@ -1,9 +1,33 @@
 require "nanoc-oo/version"
-require 'fileutils'
+require 'thor'
+
+THIS_GEM = 'nanoc-oo'
+
+class ForcedDataCopier < Thor
+  include Thor::Actions
+  class_option :force, type: :boolean, default: true
+
+  desc 'copy',''
+  def copy source, dest
+    directory source, dest
+  end
+
+  def self.source_root
+    Gem.datadir(THIS_GEM)
+  end
+end
+
+class Force < Thor
+  include Thor::Actions
+  class_option :force, type: :boolean, default: true
+
+  def self.remove_file file
+    new.remove_file file
+  end
+end
+
 
 module NanocOO
-  include FileUtils
-  
   def create_site name, blank=false
     system "nanoc create-site #{name}"
     wrap name
@@ -11,16 +35,37 @@ module NanocOO
   end
 
   def wrap dir
-    cp_r wrapper+'/.', dir
+    copier = ForcedDataCopier.new.copy 'wrapper', dir
+
+    #prepend_file join(dir,'Rules'), File.read(join(wrapper,'Rules'))
+    #directory 'lib', dir
+    #cp_r wrapper+'/.', dir
+#    act = Thor::Actions.new :force
+#    act.directory wrapper+'/.', dir
+  end
+
+  def data_dir
+    Gem.datadir('nanoc-oo')
   end
 
   def wrapper
-    File.join Gem.datadir('nanoc-oo'),  'wrapper'
+    join data_dir, 'wrapper'
   end
 
   def delete_content dir
-    rm_r Dir.glob File.join(dir,'content','*')
+    Dir.glob(join(dir,'content','*')).each do |supposedly_file|
+      Force.remove_file supposedly_file
+    end
   end
 
   extend self
+
+  # def source_root
+  #   File.expand_path(File.dirname(__FILE__))
+  # end
 end
+
+
+BEGIN{
+  def join *a; File.join *a end
+}
